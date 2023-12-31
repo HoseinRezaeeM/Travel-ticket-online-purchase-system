8770a09a5826a5229d72bfee1eb76055f33ff16c
HoseinRezaei <rezaeen77@gmail.com>
2024-01-01 01:32:43 +0330

    create CustomerServiceImpl class

---
+++ src/main/java/travelticketonlinepurchasesystem/Service/impl/CustomerServiceImpl.java
@@ -0,0 +1,19 @@
+package travelticketonlinepurchasesystem.Service.impl;
+
+import travelticketonlinepurchasesystem.Service.CustomerService;
+import travelticketonlinepurchasesystem.base.service.impl.BaseEntityServiceImpl;
+import travelticketonlinepurchasesystem.domin.Customer;
+import travelticketonlinepurchasesystem.repository.CustomerRepository;
+
+import java.util.Optional;
+
+public class CustomerServiceImpl extends BaseEntityServiceImpl<Integer, Customer, CustomerRepository> implements CustomerService {
+      public CustomerServiceImpl(CustomerRepository baseRepository) {
+            super(baseRepository);
+      }
+
+      @Override
+      public Optional<Customer> login(String username, String password) {
+            return baseRepository.login(username,password);
+      }
+}
