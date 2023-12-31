fed8bbbf1fe14296ba801972656e8f513027f453
HoseinRezaei <rezaeen77@gmail.com>
2024-01-01 01:34:06 +0330

    create TicketRepository class

---
+++ src/main/java/travelticketonlinepurchasesystem/repository/TicketRepository.java
@@ -0,0 +1,7 @@
+package travelticketonlinepurchasesystem.repository;
+
+import travelticketonlinepurchasesystem.base.repository.BaseEntityRepository;
+import travelticketonlinepurchasesystem.domin.Ticket;
+
+public interface TicketRepository extends BaseEntityRepository<Integer, Ticket> {
+}
