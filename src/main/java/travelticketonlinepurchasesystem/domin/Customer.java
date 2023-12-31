760024604b0d2d0f9107ef3993b5f53c3e44154f
HoseinRezaei <rezaeen77@gmail.com>
2024-01-01 01:32:02 +0330

    create Customer class

---
+++ src/main/java/travelticketonlinepurchasesystem/domin/Customer.java
@@ -0,0 +1,34 @@
+package travelticketonlinepurchasesystem.domin;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import travelticketonlinepurchasesystem.base.domin.BaseEntity;
+
+
+import javax.persistence.Entity;
+
+import static travelticketonlinepurchasesystem.util.PasswordGenerator.generatePassword;
+
+@Entity
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ToString
+@NoArgsConstructor
+public class Customer extends BaseEntity<Integer> {
+      String firstname;
+      String lastname;
+      String nationalCode;
+      String mobileNumber;
+      String username;
+      String password;
+
+      public Customer(String firstname, String lastname, String nationalCode, String mobileNumber) {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.nationalCode = nationalCode;
+            this.mobileNumber = mobileNumber;
+            this.username = mobileNumber;
+            this.password = generatePassword();
+      }
+}
