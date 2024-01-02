c31324e69853dd16b9b6c7fbe79912619d31603c
HoseinRezaei <rezaeen77@gmail.com>
2024-01-02 21:46:31 +0330

    create Ticket

--- src/main/java/travelticketonlinepurchasesystem/repository/TicketRepository.java
+++ src/main/java/travelticketonlinepurchasesystem/repository/TicketRepository.java
@@ -1,7 +1,12 @@
 package travelticketonlinepurchasesystem.repository;
 
 import travelticketonlinepurchasesystem.base.repository.BaseEntityRepository;
+import travelticketonlinepurchasesystem.domin.Customer;
 import travelticketonlinepurchasesystem.domin.Ticket;
+import travelticketonlinepurchasesystem.domin.Trip;
+
+import java.util.List;
 
 public interface TicketRepository extends BaseEntityRepository<Integer, Ticket> {
+      List<Ticket> findListTtripByCustomerId(Customer customer);
 }
