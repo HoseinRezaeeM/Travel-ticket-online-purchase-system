0dfcbddc65d1fab8cda31a7fc4cd816c29f6a6b1
HoseinRezaei <rezaeen77@gmail.com>
2024-01-02 21:47:01 +0330

    create TicketServiceImpl

--- src/main/java/travelticketonlinepurchasesystem/Service/impl/TicketServiceImpl.java
+++ src/main/java/travelticketonlinepurchasesystem/Service/impl/TicketServiceImpl.java
@@ -2,11 +2,20 @@ package travelticketonlinepurchasesystem.Service.impl;
 
 import travelticketonlinepurchasesystem.Service.TicketService;
 import travelticketonlinepurchasesystem.base.service.impl.BaseEntityServiceImpl;
+import travelticketonlinepurchasesystem.domin.Customer;
 import travelticketonlinepurchasesystem.domin.Ticket;
+import travelticketonlinepurchasesystem.domin.Trip;
 import travelticketonlinepurchasesystem.repository.TicketRepository;
+
+import java.util.List;
 
 public class TicketServiceImpl extends BaseEntityServiceImpl<Integer, Ticket, TicketRepository> implements TicketService {
       public TicketServiceImpl(TicketRepository baseRepository) {
             super(baseRepository);
+      }
+
+      @Override
+      public List<Ticket> findListTtripByCustomerId(Customer customer) {
+            return baseRepository.findListTtripByCustomerId(customer);
       }
 }
