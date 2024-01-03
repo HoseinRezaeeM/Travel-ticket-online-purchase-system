317aafdcdfb913751c4fc0952b17847253255b75
HoseinRezaei <rezaeen77@gmail.com>
2024-01-03 14:24:39 +0330

    create TicketRepositoryImpl

--- src/main/java/travelticketonlinepurchasesystem/repository/impl/TicketRepositoryImpl.java
+++ src/main/java/travelticketonlinepurchasesystem/repository/impl/TicketRepositoryImpl.java
@@ -7,7 +7,10 @@ import travelticketonlinepurchasesystem.domin.Trip;
 import travelticketonlinepurchasesystem.repository.TicketRepository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import java.util.List;
+import java.util.Optional;
+
 @SuppressWarnings("unchecked")
 public class TicketRepositoryImpl extends BaseEntityRepositoryImpl<Integer, Ticket> implements TicketRepository {
       public TicketRepositoryImpl(EntityManager entityManager) {
@@ -25,5 +28,18 @@ public class TicketRepositoryImpl extends BaseEntityRepositoryImpl<Integer, Tick
                    .createQuery("from Ticket t WHERE t.customer=:customer")
                    .setParameter("customer",customer)
                    .getResultList();
+      }
+
+      @Override
+      public Optional<Ticket> findTicketByTicketId(int id) {
+            try {
+                  return Optional.ofNullable(
+                         entityManager.createQuery("FROM Ticket t FULL JOIN Trip tr on t.trip.id=tr.id"
+                                                   + " where t.id=:id ", Ticket.class)
+                                .setParameter("id", id)
+                                .getSingleResult());
+            } catch (NoResultException e) {
+                  return Optional.empty();
+            }
       }
 }
