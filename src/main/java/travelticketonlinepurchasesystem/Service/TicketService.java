5a626cbb945c73d6b0570bb7d3d7c426c132bcaa
HoseinRezaei <rezaeen77@gmail.com>
2024-01-03 14:24:55 +0330

    create TicketService

--- src/main/java/travelticketonlinepurchasesystem/Service/TicketService.java
+++ src/main/java/travelticketonlinepurchasesystem/Service/TicketService.java
@@ -6,9 +6,11 @@ import travelticketonlinepurchasesystem.domin.Ticket;
 import travelticketonlinepurchasesystem.domin.Trip;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TicketService extends BaseEntityService<Integer, Ticket> {
       List<Ticket> findListTtripByCustomerId(Customer customer);
+      Optional<Ticket> findTicketByTicketId(int id);
 }
 
 
