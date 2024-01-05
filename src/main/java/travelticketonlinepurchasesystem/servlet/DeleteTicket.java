d9784e702eef858956e7d297a9da074a24898b02
HoseinRezaei <rezaeen77@gmail.com>
2024-01-05 18:56:21 +0330

    create DeleteTicket

---
+++ src/main/java/travelticketonlinepurchasesystem/servlet/DeleteTicket.java
@@ -0,0 +1,30 @@
+package travelticketonlinepurchasesystem.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import travelticketonlinepurchasesystem.domin.Ticket;
+import travelticketonlinepurchasesystem.util.ApplicationContex;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet("/del")
+public class DeleteTicket extends HttpServlet {
+      @Override
+      protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            HttpSession session = req.getSession();
+            int delid = (int) session.getAttribute("delid");
+            ApplicationContex.getTicketServiceImpl().deleteById(delid);
+            Optional<Ticket> id = ApplicationContex.getTicketServiceImpl().findById(delid);
+            if(id.isEmpty()){
+                  RequestDispatcher dispatcher = req.getRequestDispatcher("deletesucess.jsp");
+                  dispatcher.forward(req, resp);
+            }
+
+      }
+}
