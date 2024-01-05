4a8c75253e8f5d01ca866f4ac98be22bfe9bd907
HoseinRezaei <rezaeen77@gmail.com>
2024-01-05 19:26:04 +0330

    add FileOutputStream for save image in file

--- src/main/java/travelticketonlinepurchasesystem/servlet/RegisterSerlvlet.java
+++ src/main/java/travelticketonlinepurchasesystem/servlet/RegisterSerlvlet.java
@@ -11,6 +11,8 @@ import org.apache.commons.compress.utils.IOUtils;
 import travelticketonlinepurchasesystem.domin.Customer;
 import travelticketonlinepurchasesystem.util.ApplicationContex;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Optional;
@@ -22,22 +24,32 @@ public class RegisterSerlvlet extends HttpServlet {
       protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
             String firstname = req.getParameter("firstname");
-            String lastname= req.getParameter("lastname");
+            String lastname = req.getParameter("lastname");
             String nationalCode = req.getParameter("nationalCode");
             String mobileNumber = req.getParameter("mobileNumber");
 
             Part filePart = req.getPart("pic");  //Add image
             InputStream fileContent = filePart.getInputStream();
 
-            Customer customer =new Customer(firstname,lastname,nationalCode,mobileNumber, IOUtils.toByteArray(fileContent));
+            Customer customer = new Customer(firstname, lastname, nationalCode, mobileNumber, IOUtils.toByteArray(fileContent));
             final Customer save = ApplicationContex.getCustomerServiceImpl().save(customer);
+            saveImageToFile(save.getPhotoIdCard(), "C:\\Users\\sp\\IdeaProjects\\travel-ticket-online-purchase\\src\\main\\webapp\\uploaded\\uploaded.jpg");
+
             final Optional<Customer> customerOptional = ApplicationContex.getCustomerServiceImpl().findById(save.getId());
-            if(customerOptional.isPresent()){
+            if (customerOptional.isPresent()) {
                   resp.sendRedirect("login.jsp");
-            }
-            else{
+            } else {
                   resp.sendRedirect("register.jsp");
             }
 
+      }
+
+
+      public static void saveImageToFile(byte[] imageBytes, String filePath) {
+            try (FileOutputStream fileOutputStream = new FileOutputStream(filePath)) {
+                  fileOutputStream.write(imageBytes);
+            } catch (IOException e) {
+                  e.printStackTrace();
+            }
       }
 }
