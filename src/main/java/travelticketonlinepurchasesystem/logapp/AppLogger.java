f93fee74c82dda6b7e70d472a92215fa98b681e8
HoseinRezaei <rezaeen77@gmail.com>
2024-01-05 19:25:00 +0330

    create AppLogger

---
+++ src/main/java/travelticketonlinepurchasesystem/logapp/AppLogger.java
@@ -0,0 +1,11 @@
+package travelticketonlinepurchasesystem.logapp;
+
+
+import java.util.logging.Logger;
+
+public class AppLogger {
+
+      private final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+
+}
