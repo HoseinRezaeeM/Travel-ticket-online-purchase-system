13ce60e56f686b9fa914aa0425785054c1b59d80
HoseinRezaei <rezaeen77@gmail.com>
2024-01-06 12:31:46 +0330

    create BankCardRepository

---
+++ src/main/java/travelticketonlinepurchasesystem/repository/BankCardRepository.java
@@ -0,0 +1,7 @@
+package travelticketonlinepurchasesystem.repository;
+
+import travelticketonlinepurchasesystem.base.repository.BaseEntityRepository;
+import travelticketonlinepurchasesystem.domin.BankCard;
+
+public interface BankCardRepository extends BaseEntityRepository<Integer, BankCard> {
+}
