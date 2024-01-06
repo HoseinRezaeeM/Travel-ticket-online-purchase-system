30f0a32a5e352e01ed5a9d55e519a2a6ae0825e3
HoseinRezaei <rezaeen77@gmail.com>
2024-01-06 12:32:03 +0330

    create BankCardService

---
+++ src/main/java/travelticketonlinepurchasesystem/Service/BankCardService.java
@@ -0,0 +1,7 @@
+package travelticketonlinepurchasesystem.Service;
+
+import travelticketonlinepurchasesystem.base.service.BaseEntityService;
+import travelticketonlinepurchasesystem.domin.BankCard;
+
+public interface BankCardService extends BaseEntityService<Integer, BankCard> {
+}
